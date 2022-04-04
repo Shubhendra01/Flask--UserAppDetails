8c2a67a31b063dd3ee519c44aa14dc502917b2ba
unknown <shubhhhh17@gmail.com>
2022-04-04 23:00:27 +0530

    Flask Student User App Details Web Page created Successfully

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def slash():
+    return render_template("slash.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run()
